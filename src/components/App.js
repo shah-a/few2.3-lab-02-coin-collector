c184da2130072d050a1533537f392f30b8a65e9a
Ali Shah <s.ashah@outlook.com>
2021-09-03 21:23:56 -0400

    [Update] used object for state

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,33 @@
 import './App.css';
+import coinData from '../coins.json';
 
 import { useState } from 'react';
 
 function App() {
-  const [pennies, setPennies] = useState(0);
-  const [nickles, setNickles] = useState(0);
-  const [dimes, setDimes] = useState(0);
-  const [quarters, setQuarters] = useState(0);
+  const [coins, setCoins] = useState(coinData);
 
-  const total =
-    (pennies * 0.01) +
-    (nickles * 0.05) +
-    (dimes * 0.10) +
-    (quarters * 0.25);
+  const total = coins.reduce((acc, coin) => {
+    return acc + (coin.value * coin.quantity);
+  }, 0);
+
+  const buttons = coins.map((coin, index) => {
+    return (
+      <button
+        onClick={() => {
+          const newCoins = [...coins];
+          newCoins[index].quantity += 1;
+          setCoins(newCoins);
+        }}
+      >+${coin.value.toFixed(2)}</button>
+    );
+  });
 
   return (
     <div className="App">
       <h1>Coin Collector!</h1>
       <h2>Total: ${total.toFixed(2)}</h2>
-      <p>Pennies: {pennies} | Nickles: {nickles} | Dimes: {dimes} | Quarters: {quarters}</p>
-
-      <button
-        onClick={() => setPennies(pennies + 1)}
-      >+$0.01</button>
-
-      <button
-        onClick={() => setNickles(nickles + 1)}
-      >+$0.05</button>
-
-      <button
-        onClick={() => setDimes(dimes + 1)}
-      >+$0.10</button>
-
-      <button
-        onClick={() => setQuarters(quarters + 1)}
-      >+$0.25</button>
+      <p>Pennies: {coins[0].quantity} | Nickles: {coins[1].quantity} | Dimes: {coins[2].quantity} | Quarters: {coins[3].quantity}</p>
+      {buttons}
     </div>
   );
 }
